ac25921539a595c55eb6b8e59d8222a517834dfe
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-01-24 10:05:29 +0700

    get parahraph from word and split word into array and split 2d array to 1d array

--- import_docx.py
+++ import_docx.py
@@ -2,6 +2,7 @@ from docx import Document
 import re
 import string
 dokumen = Document('CV.docx')
+import numpy as np
 
 par_array = []
 par = dokumen.paragraphs
@@ -12,7 +13,19 @@ for para in par:
     remove_punctuation = remove_angka.translate(str.maketrans("","",string.punctuation))
     par_array.append(remove_punctuation.split())
 
+tokenize = []
 for i in par_array:
     if len(i) > 0:
-        print(i)
+        tokenize.append(i)
+
+list_par = []
+for i in range(len(tokenize)):
+    for a in range(len(tokenize[i])):
+        list_par.append(tokenize[i][a])
+
+print(list_par)
+
+
+
+
 

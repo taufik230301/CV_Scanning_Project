d5d8ce11304f7de3e63b7f1185f7d41473a852ac
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-01-24 10:20:11 +0700

    get parahraph from word and split word into array and split 2d array to 1d array

--- import_docx.py
+++ import_docx.py
@@ -1,10 +1,18 @@
 from docx import Document
 import re
 import string
-dokumen = Document('CV.docx')
+
+# import numpy
 import numpy as np
 
+# import word_tokenize & FreqDist from NLTK
+from nltk.tokenize import word_tokenize 
+from nltk.probability import FreqDist
+
+dokumen = Document('CV.docx')
+
 par_array = []
+
 par = dokumen.paragraphs
 
 for para in par:
@@ -13,17 +21,19 @@ for para in par:
     remove_punctuation = remove_angka.translate(str.maketrans("","",string.punctuation))
     par_array.append(remove_punctuation.split())
 
-tokenize = []
-for i in par_array:
-    if len(i) > 0:
-        tokenize.append(i)
+print(par_array)
 
-list_par = []
-for i in range(len(tokenize)):
-    for a in range(len(tokenize[i])):
-        list_par.append(tokenize[i][a])
+# tokenize = []
+# for i in par_array:
+#     if len(i) > 0:
+#         tokenize.append(i)
 
-print(list_par)
+# list_par = []
+# for i in range(len(tokenize)):
+#     for a in range(len(tokenize[i])):
+#         list_par.append(tokenize[i][a])
+
+# print(list_par)
 
 
 

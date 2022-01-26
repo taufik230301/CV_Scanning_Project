a750c671beb08e7f5503f0fff0a08477100a2f44
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-01-26 07:04:04 +0700

    try to remove punctuation

--- 01_case_folding.py
+++ 01_case_folding.py
@@ -7,3 +7,13 @@ kalimat = "Saya belajar NLTK :)"
 lowe_case = kalimat.lower()
 
 print(lowe_case)
+
+# Sebelum Case Folding
+print('Sebelum Case Folding')
+kalimat = "Saya belajar NLTK :)"
+print(kalimat)
+
+# Case Folding
+lower_sentence = kalimat.lower()
+print('Hasil dari Case folding')
+print(lower_sentence)
--- 02_Tokenizing.py
+++ 02_Tokenizing.py
@@ -1,12 +1,19 @@
-import nltk
+import string
+import re
+from nltk.tokenize import word_tokenize
 
 
-# Sebelum Case Folding
-print('Sebelum Case Folding')
-kalimat = "Saya belajar NLTK :)"
-print(kalimat)
+sentence = """Obat berbentuk pil buatan Pfizer Inc. bernama Paclovid. Saat ini, 
+pihak Pfizer berencana untuk menyerahkan data sesegera mungkin kepada Food and Drug Administration (FDA) 
+untuk meminta otorisasi agar obat tersebut digunakan di Amerika Serikat. Pil Pfizer telah diuji pada pasien berisiko 
+tinggi, berisiko rendah, dan orang-orang yang tinggal serumah dengan pasien terkonfirmasi positif COVID-19."""
 
 # Case Folding
-lower_sentence = kalimat.lower()
-print('Hasil dari Case folding')
-print(lower_sentence)
+lower_sentence = sentence.lower()
+
+
+# Tokenizing
+tokenize_sentence = re.sub(r"\+d", "", lower_sentence)
+
+
+print(tokenize_sentence)

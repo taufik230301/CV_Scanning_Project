01552c108198b656d184f5c83fdd3bf77404d6fb
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-01-26 09:22:17 +0700

    tokenize to array

--- 04_remove_whitespace.py
+++ 04_remove_whitespace.py
@@ -19,6 +19,9 @@ tokenize_sentence = re.sub(r"\+d", "", lower_sentence)
 # Remove Punctutation
 remove_punctuation_sentence = tokenize_sentence.translate(str.maketrans("","",string.punctuation))
 
+
+tokens = nltk.tokenize.word_tokenize(remove_punctuation_sentence)
+
 # remove white space
 whitespace_remove_sentence = remove_punctuation_sentence.strip()
 print("Setelah meremove whitespace")
@@ -29,3 +32,7 @@ multiple_whitespace_sentence = re.sub('\s+',' ',whitespace_remove_sentence)
 print("Setelah meremove multiple whitespace")
 print(" ")
 print(multiple_whitespace_sentence)
+print("-"*200)
+print("Tokenize to Array")
+print(" ")
+print(tokens)
